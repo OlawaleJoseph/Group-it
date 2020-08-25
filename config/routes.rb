892e556303090bad4c46b38e4bcf9c168735546f
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-25 16:31:38 -0700

    add group, expenses, external resources  routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :groups
+  resources :expenses
   resources :users, except: [:new]
   root 'welcome#index'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
+  get 'external', :to => "expenses#external"
 end
