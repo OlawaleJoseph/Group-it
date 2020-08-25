d66835c3f5f215044543ca6a91a0ba1d9843fcd6
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-25 12:44:59 -0700

    add routing for login, log out and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, except: [:new]
   root 'welcome#index'
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 end
