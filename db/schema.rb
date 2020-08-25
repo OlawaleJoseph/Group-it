4980f031b556c1c24ff65bc0d257a351fa121bf6
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-25 12:45:27 -0700

    fix schema linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_165040) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_200_825_165_040) do
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
