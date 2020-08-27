0668e8a2712aa2ad8dfa5a9d1f538bc1d75cfbac
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-26 17:02:53 -0700

    add expense model validation  for name and amount

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,8 @@ class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :expense_groups
   has_many :groups, through: :expense_groups
+
+  validates :name, presence: true, length: { minimum: 3, maximun: 25 }
+  validates_numericality_of :amount, greater_than: 0, allow_nil: true
+
 end
