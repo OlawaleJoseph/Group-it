8304cffee82f6d6ddcccaf714095ec46e6c3da0b
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-30 14:53:43 -0700

    add the has_one_attached to group model for image

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :expense_groups
   has_many :expenses, through: :expense_groups
+  has_one_attached :image
 
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
 
