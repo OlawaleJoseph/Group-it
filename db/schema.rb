1f45ed83c0b88eed0faf18bd0421ae1108c74e95
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-30 14:53:09 -0700

    migrate database to update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_825_231_130) do
-  create_table 'expense_groups', force: :cascade do |t|
-    t.integer 'expense_id', null: false
-    t.integer 'group_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['expense_id'], name: 'index_expense_groups_on_expense_id'
-    t.index ['group_id'], name: 'index_expense_groups_on_group_id'
+ActiveRecord::Schema.define(version: 2020_08_30_214906) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'expenses', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'amount'
-    t.integer 'author_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['author_id'], name: 'index_expenses_on_author_id'
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'groups', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'author_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "expense_groups", force: :cascade do |t|
+    t.integer "expense_id", null: false
+    t.integer "group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["expense_id"], name: "index_expense_groups_on_expense_id"
+    t.index ["group_id"], name: "index_expense_groups_on_group_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.integer "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
   end
 
-  add_foreign_key 'expense_groups', 'expenses'
-  add_foreign_key 'expense_groups', 'groups'
-  add_foreign_key 'expenses', 'users', column: 'author_id'
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.integer "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "expense_groups", "expenses"
+  add_foreign_key "expense_groups", "groups"
+  add_foreign_key "expenses", "users", column: "author_id"
 end
