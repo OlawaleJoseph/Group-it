3c434fd6e65639967a5800337eb333ad5afa0e27
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-30 17:55:57 -0700

    build expenses helper methods

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,9 @@
 module ExpensesHelper
+    def display_expenses(name, expenses)
+        render partial: name, locals: { obj: expenses} if expenses
+      end
+
+      def display_image(expense)
+        "<div class='col-sm-2'> #{image_tag(expense.groups.first.image, class: 'group-img' )}</div>".html_safe if expense.groups.exists? 
+      end
 end
